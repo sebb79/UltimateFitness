afb97e815e6b7f3591e42d199995861c77420316
sebb79 <sebastien_besse@yahoo.com>
2023-06-12 11:47:43 +0200

    r

--- src/main/java/com/ultimatefitness/ultimateFitness/controller/UserController.java
+++ src/main/java/com/ultimatefitness/ultimateFitness/controller/UserController.java
@@ -1,17 +1,13 @@
 package com.ultimatefitness.ultimateFitness.controller;
 
 
-import model.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
-
-    private final User users;
-
-
 
     @RequestMapping("/hello")
     @ResponseBody
